87fbdd05a59b6f7ce80102ad968e727d372caf05
Gavin Morgan <morgan.gavin@gmail.com>
2022-11-16 15:52:54 -0800

    tetrahedrons

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,52 @@ import { Suspense } from "react";
 
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 
-import { Html, MeshDistortMaterial, MeshReflectorMaterial, Plane,  } from '@react-three/drei';
+import {
+  Html,
+  MeshDistortMaterial,
+  MeshReflectorMaterial,
+  Plane,
+} from "@react-three/drei";
 import { OrbitControls } from "@react-three/drei";
 
 import Helpers from "./components/helpers";
 import Objects from "./components/objects";
 
-const Loading = <Html><div>LOADING...</div></Html>;
+const Loading = (
+  <Html>
+    <div>LOADING...</div>
+  </Html>
+);
 
 const aspect = window.innerWidth / window.innerHeight;
 const d = 20;
 
 export default function App() {
   return (
-    <Canvas camera={{left: -d * aspect, right: d * aspect, top: d, bottom: -d, near:1,far:1000}}>
-      
-      <Suspense fallback={ Loading }>
-
+    <Canvas
+      style={{ background: "#191B22" }}
+      camera={{
+        left: -d * aspect,
+        right: d * aspect,
+        top: d,
+        bottom: -d,
+        near: 1,
+        far: 1000,
+      }}
+    >
+      <Suspense fallback={Loading}>
         <Helpers />
         <OrbitControls />
 
         <Objects />
 
-        <Plane args={ [ 100, 100 ] }
-          position={ [ 0, -1, 0 ] }
-          rotation={ [ -1.57, 0, 0 ] }>
+        <Plane args={[100, 100]} position={[0, -1, 0]} rotation={[-1.57, 0, 0]}>
           <meshStandardMaterial color="#282c37" />
         </Plane>
 
-        <ambientLight args={ [ 0xffffff ] } intensity={ 0.5 } />
-        <directionalLight position={ [ 1, 2, 1 ] } intensity={ 1 } />
-
+        <ambientLight args={[0xffffff]} intensity={0.5} />
+        <directionalLight position={[1, 2, 1]} intensity={1} />
       </Suspense>
     </Canvas>
   );
 }
-
--- src/components/objects.tsx
+++ src/components/objects.tsx
@@ -1,34 +1,60 @@
-import { MeshDistortMaterial, MeshReflectorMaterial, Plane, RoundedBox, Sphere } from '@react-three/drei';
-import { useFrame, useThree } from '@react-three/fiber'
+import { RoundedBox, Tetrahedron } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useControls } from "leva";
+import { useRef } from "react";
 
-const palettes = [`#FFAE1C
+const palettes = [
+  `#FFAE1C
 #BB160F
 #23D7F3
 #00461D
 #FF6FFF
-#0059AB`.split("\n")]
+#0059AB`.split("\n"),
+];
 
-export default function Objects() {
+const shapes = [d4, d6];
 
-    useFrame((state, delta) => {
-        console.log(delta)
-    })
+export default function Objects(props: any) {
+  //   const ref = useRef();
 
-    useThree(({camera, scene}) => {
-        camera.position.set( -5, 5, 5 ); // all components equal
-        camera.lookAt( scene.position );
-      });
-      
+  // const { angleX, angleY, angleZ } = useControls({
+  //   angleX: -0.6,
+  //   angleY: 0.7,
+  //   angleZ: 0.79,
+  // });
 
-    return (
-        <>
-        {
-        palettes.map(palette => palette.map((color, i) => {
-            return <RoundedBox args={[1, 1, 1]} position={[1.25*i-3.25, 0, 0]}>
-                <meshStandardMaterial color={color} />
-            </RoundedBox>
-        }))
-        }
-        </>
-    )
+  useThree(({ camera, scene }) => {
+    camera.position.set(-5, 5, 5); // all components equal
+    camera.lookAt(scene.position);
+  });
+
+  return (
+    <>
+      {palettes.map((palette, z) =>
+        palette.map((color, x) => {
+          return shapes[x % 2](x, color);
+        })
+      )}
+    </>
+  );
+}
+function d4(x: number, color: string): JSX.Element {
+  return (
+    <Tetrahedron
+      args={[0.7, 0]}
+      rotation={[-0.6, 0, 0.79]}
+      position={[1.5 * x - 3.25, -0.25, 0]}
+      key={color}
+    >
+      <meshStandardMaterial color={color} />
+    </Tetrahedron>
+  );
+}
+
+function d6(x: number, color: string): JSX.Element {
+  return (
+    <RoundedBox args={[1, 1, 1]} position={[1.5 * x - 3.25, 0, 0]} key={color}>
+      <meshStandardMaterial color={color} />
+    </RoundedBox>
+  );
 }
